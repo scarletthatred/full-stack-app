e6941ba5b7777a9a428d6b045d13b8b407d26116
Student <>
2024-05-13 15:02:42 -0500

    dropdown menus added to the application
    added the dropdown menus to the application that loop through the options to build a list

--- ch07_ex2/index.php
+++ ch07_ex2/index.php
@@ -21,12 +21,17 @@
 
         <div id="data">
             <label>Investment Amount:</label>
-            <input type="text" name="investment"
-                   value="<?php echo $investment; ?>"/><br>
+            <select id="dropdown1" name="investment">
+             
+            </select>
+            <br>
 
-            <label>Yearly Interest Rate:</label>
-            <input type="text" name="interest_rate"
-                   value="<?php echo $interest_rate; ?>"/><br>
+            <label for="dropdown2">Yearly Interest Rate:</label>
+            <select id="dropdown2" name="interest_rate">
+                
+            
+            </select>
+           <br>
 
             <label>Number of Years:</label>
             <input type="text" name="years"
@@ -39,6 +44,22 @@
         </div>
 
     </form>
+    <script>
+                    var select =document.getElementById("dropdown2")
+                    var select2 = document.getElementById("dropdown1")
+                    for(var i=4; i<=12;i = i+0.5){
+                        var option = document.createElement("option");
+                        option.value = i;
+                        option.text = i;
+                        select.appendChild(option);
+                    }
+                    for(var j=10;j<=50;j = j+5){
+                        var option2 = document.createElement("option");
+                        option2.value = j*1000;
+                        option2.text = j*1000;
+                        select2.appendChild(option2);
+                    }
+                </script>
     </main>
 </body>
 </html>
